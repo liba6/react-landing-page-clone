f786c2100014eb8744fbfc54ca4bf6cfaabc019a
liba-ny <94902436+liba-ny@users.noreply.github.com>
2023-01-24 00:01:12 +0100

    Edits navbar

--- src/App.js
+++ src/App.js
@@ -4,66 +4,97 @@ import { css } from '@emotion/react';
 import logo from './logo.svg';
 
 const navBar = css`
-  //background-color: white;
+  background-color: lavenderblush;
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: space-around;
   margin: 14px 0;
   padding: 14px 21px;
-  background-color: transparent;
   border-radius: 8px;
 `;
-const centerImage = css`
-  display: inline-block;
-  max-height: 280px;
-  justify-content: center;
-`;
+
 const navBarTabs = css`
   display: flex;
   flex-direction: row;
+  justify-content: space-between;
+  gap: 22px;
+`;
+const flamingPricing = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 const rightNav = css`
-  gap: 15px;
+  display: flex;
+  //justify-content: space-between;
+  gap: 40px;
+`;
+const login = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const navbutton = css`
+  padding: 9px 21px;
+  border-radius: 10px;
+  border: 2px solid #0f0f0f;
+  background-color: transparent;
+  color: #0f0f0f;
 `;
 const centerSection = css`
   text-align: center;
   padding-top: 112px;
   padding-bottom: 112px;
 `;
-const rivermate = css`
-  font-family: sans-serif;
-  font-size: 28px;
-  color: #333333;
-`;
-const button = css`
-  padding: 14px 28px;
-  border-radius: 10px;
+
+const centerImage = css`
+  display: inline-block;
+  max-height: 280px;
+  justify-content: center;
 `;
 
 function App() {
   return (
     <div>
       <nav css={navBar}>
-        <div css={rivermate}>rivermate</div>
+        <img
+          src="https://uploads-ssl.webflow.com/627275b349f60885b6c33689/6276d5749c10c960bc0422a4_rivermate.svg"
+          height="22"
+          alt="rivermate logo"
+        />
         <div css={navBarTabs}>
-          <p>How it Works </p>
           <p>
-            Pricing <span>🔥</span>
+            How it Works
+            <img
+              src="https://uploads-ssl.webflow.com/627275b349f60885b6c33689/627275b349f6086248c336f5_icon-chevron-down.svg"
+              alt="dropdown toggle"
+            ></img>
           </p>
+          <section css={flamingPricing}>
+            <p>
+              Pricing <span>🔥</span>
+            </p>
+          </section>
           <p>Countries</p>
           <p>Partners</p>
-          <p>Company</p>
+          <p>
+            Company{' '}
+            <img
+              src="https://uploads-ssl.webflow.com/627275b349f60885b6c33689/627275b349f6086248c336f5_icon-chevron-down.svg"
+              alt="dropdown toggle"
+            ></img>
+          </p>
         </div>
-        <div scc={rightNav}>
-          Login
-          <img
-            src="https://uploads-ssl.webflow.com/627275b349f60885b6c33689/627275b349f6082e5ac336f0_icon-arrow-up-right.svg"
-            loading="eager"
-            alt=""
-            class="arrow-link-icon"
-          ></img>
-          <button css={button}>Get Started</button>
-          <button css={button}>Book A Call </button>
+        <div css={rightNav}>
+          <div css={login}>
+            Login
+            <img
+              src="https://uploads-ssl.webflow.com/627275b349f60885b6c33689/627275b349f6082e5ac336f0_icon-arrow-up-right.svg"
+              alt="upward arrow"
+            ></img>
+          </div>
+          <button css={navbutton}>Get Started</button>
+          <button css={navbutton}>Book A Call </button>
         </div>
       </nav>
       <section css={centerSection}>
@@ -77,8 +108,8 @@ function App() {
         ></img>
         <h1>Hire anyone, anywhere.</h1>
         <h2>Hire remote employees anywhere in the world.</h2>
-        <button css={button}>Get started</button>
-        <button css={button}>Book a call</button>
+        <button>Get started</button>
+        <button>Book a call</button>
       </section>
     </div>
   );
